b44b382bfb60dcab88a142c1171f85af24d60579
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-06-11 13:48:40 -0700

    Implement snake game logic.

---
+++ include/game/game.hpp
@@ -0,0 +1,113 @@
+#ifndef GAME_
+#define GAME_
+
+#include <vector>
+
+namespace snake {
+namespace game {
+
+enum class Direction {
+    kUp,
+    kDown,
+    kLeft,
+    kRight,
+    kNone,
+};
+
+struct ScreenDimension {
+    int width = 0;
+    int height = 0;
+};
+
+/**
+ * Representation of a single location on the screen.
+ *
+ * The screen is divided up into a grid of 2D coordinate locations called Tiles.
+ * Tiles can optionally have a direction when they are associated with the
+ * snake.
+ */
+struct Tile {
+    int row = 0;
+    int col = 0;
+    Direction direction = Direction::kNone;
+
+    /* it's hacky but we exclude the direction in the equality comparison
+     * because we want to be able to compare snake tiles with target tiles which
+     * have no direction */
+    friend bool operator==(const Tile& a, const Tile& b) {
+        return ((a.row == b.row) && (a.col == b.col));
+    }
+};
+
+using Snake = std::vector<Tile>;
+using Targets = std::vector<Tile>;
+
+class SnakeGame {
+   public:
+    /** Spawn a snake and target on a screen with the parameter dimensions. */
+    explicit SnakeGame(const ScreenDimension& dim);
+
+    SnakeGame() = delete;
+    ~SnakeGame() = default;
+    SnakeGame(const SnakeGame&) = default;
+    SnakeGame& operator=(const SnakeGame&) = default;
+    SnakeGame(SnakeGame&&) = default;
+    SnakeGame& operator=(SnakeGame&&) = default;
+
+    int GetScore() const { return score_; }
+    const Tile& GetTargetTile() const { return targets_[curr_target_]; }
+    const Snake& GetSnake() const { return snake_; }
+    bool GameOver() const { return game_over_; }
+
+    /**
+     * Advance the snake one game tick in the parameter direction.
+     *
+     * If the game has ended, Tick() will do nothing.
+     *
+     * @param[in] new_direction Direction in which the snake shall move in this
+     * game tick.
+     */
+    void Tick(const Direction& new_direction);
+
+    /** Reset game state and spawn a new snake and target. */
+    void Reset();
+
+   private:
+    static const int kScoreIncrement = 10;
+
+    /** Spawn a single snake head Tile with a random direction at the screen
+     * center. */
+    void SpawnSnake();
+
+    /** Advance the snake one Tile in the parameter direction. */
+    void MoveSnake(const Direction& new_direction);
+
+    /** Append a new snake Tile to the end of the snake. */
+    void ExtendSnake();
+
+    /**
+     * Return true if the game has ended in a loss.
+     *
+     * The game can end under the following conditions:
+     *   (1) The snake head has intersected the snake body at some Tile.
+     *   (2) The snake has attempted to go beyond the screen bounds.
+     *
+     * @returns true if the game has ended in a loss.
+     */
+    bool IsGameOver() const;
+
+    /** Return true if the snake has won by populating every screen Tile. */
+    bool SnakeWins() const;
+
+    bool game_over_;
+    int score_;
+    ScreenDimension screen_dim_;
+    Snake snake_;
+    int curr_target_;
+    Targets targets_;
+};
+
+}  // namespace game
+}  // namespace snake
+
+#endif
---
+++ src/game/game.cc
@@ -0,0 +1,174 @@
+#include "game/game.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <random>
+
+namespace snake {
+namespace game {
+
+void SnakeGame::SpawnSnake() {
+    /* create a random shuffle of the possible directions the snake can go */
+    std::vector<Direction> directions = {Direction::kUp, Direction::kDown,
+                                         Direction::kLeft, Direction::kRight};
+    auto rd = std::random_device{};
+    auto rng = std::default_random_engine{rd()};
+    std::shuffle(directions.begin(), directions.end(), rng);
+
+    /* spawn the snake head in the center of the screen with a random direction
+     */
+    snake_.push_back({.row = screen_dim_.height / 2,
+                      .col = screen_dim_.width / 2,
+                      .direction = directions[0]});
+}
+
+void SnakeGame::MoveSnake(const Direction& new_direction) {
+    /* shift all but the head tiles into their predecessor's position */
+    Snake tmp = snake_;
+    for (std::size_t i = 1; i < snake_.size(); ++i) {
+        snake_[i] = tmp[i - 1];
+    }
+
+    /* walk the head forward in whatever direction it's facing */
+    Tile& head = snake_.front();
+    head.direction = new_direction;
+    switch (snake_.front().direction) {
+        case Direction::kUp:
+            head.row--;
+            break;
+        case Direction::kDown:
+            head.row++;
+            break;
+        case Direction::kLeft:
+            head.col--;
+            break;
+        case Direction::kRight:
+            head.col++;
+            break;
+        case Direction::kNone:
+            break;
+    }
+}
+
+void SnakeGame::ExtendSnake() {
+    Tile new_snake_tile = snake_.back();
+
+    /* the new tile's location is the current snake tail's location shifted
+     * opposite the snake tail's direction */
+    switch (new_snake_tile.direction) {
+        case Direction::kUp:
+            new_snake_tile.row++;
+            break;
+        case Direction::kDown:
+            new_snake_tile.row--;
+            break;
+        case Direction::kLeft:
+            new_snake_tile.col++;
+            break;
+        case Direction::kRight:
+            new_snake_tile.col--;
+            break;
+        case Direction::kNone:
+            break;
+    }
+    snake_.push_back(new_snake_tile);
+}
+
+bool SnakeGame::IsGameOver() const {
+    /* check if the snake overlaps itself at any tile */
+    for (std::size_t i = 0; i < snake_.size(); ++i) {
+        for (std::size_t j = i + 1; j < snake_.size(); ++j) {
+            if (snake_[i] == snake_[j]) {
+                return true;
+            }
+        }
+    }
+
+    /* verify the head snake tile is in bounds */
+    bool is_in_row_bounds =
+        (snake_.front().row >= 0) && (snake_.front().row < screen_dim_.height);
+    bool is_in_col_bounds =
+        (snake_.front().col >= 0) && (snake_.front().col < screen_dim_.width);
+
+    return (!is_in_row_bounds || !is_in_col_bounds);
+}
+
+bool SnakeGame::SnakeWins() const {
+    /* check whether the snake is occupying every possible target location */
+    for (const Tile& target_tile : targets_) {
+        bool found = false;
+        for (const Tile& snake_tile : snake_) {
+            if (snake_tile == target_tile) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) { /* looks like there's at least one open target location */
+            return false;
+        }
+    }
+    return true;
+}
+
+SnakeGame::SnakeGame(const ScreenDimension& dim)
+    : game_over_(false), score_(0), screen_dim_(dim), curr_target_(0) {
+    Reset();
+}
+
+void SnakeGame::Tick(const Direction& new_direction) {
+    MoveSnake(new_direction);
+
+    if (IsGameOver()) { /* do nothing if the game has already ended */
+        game_over_ = true;
+        return;
+    }
+
+    /* looks like the snake ate its target */
+    if (snake_[0] == targets_[curr_target_]) {
+        score_ += kScoreIncrement;
+
+        ExtendSnake();
+
+        if (SnakeWins()) {
+            game_over_ = true;
+            return;
+        }
+
+        /* search for the next target tile that is not occupied by the snake */
+        while (std::find(snake_.begin(), snake_.end(),
+                         targets_[curr_target_]) != snake_.end()) {
+            curr_target_ =
+                (curr_target_ + 1) % static_cast<int>(targets_.size());
+        }
+    }
+}
+
+void SnakeGame::Reset() {
+    game_over_ = false;
+    score_ = 0;
+
+    /* generate a randomly shuffled vector of potential target locations */
+    targets_.clear();
+    for (int i = 0; i < screen_dim_.height; ++i) {
+        for (int j = 0; j < screen_dim_.width; ++j) {
+            targets_.push_back(
+                {.row = i, .col = j, .direction = Direction::kNone});
+        }
+    }
+    auto rd = std::random_device{};
+    auto rng = std::default_random_engine{rd()};
+    std::shuffle(targets_.begin(), targets_.end(), rng);
+    curr_target_ = 0;
+
+    /* respawn the snake */
+    snake_.clear();
+    SpawnSnake();
+
+    /* ensure the target does not overlap the snake head */
+    while (targets_[curr_target_] == snake_.front()) {
+        curr_target_ = (curr_target_ + 1) % static_cast<int>(targets_.size());
+    }
+}
+
+}  // namespace game
+}  // namespace snake
