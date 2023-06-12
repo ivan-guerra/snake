d9e7fd23798971f18385f393dffeb1dc42e7ea3d
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-06-11 17:58:08 -0700

    Implement the main game loop.

--- src/snake/snake.cc
+++ src/snake/snake.cc
@@ -1,21 +1,38 @@
-#include <ncurses.h>
+#include "game/game.hpp"
+#include "graphics/screen.hpp"
 
-#include <string>
+void RunGameLoop(snake::game::SnakeGame& game) {
+    /* setup to timeout on input after kReadDelayMs milliseconds */
+    const int kReadDelayMs = 100;
+    snake::graphics::EnableInputDelay(kReadDelayMs);
+
+    snake::game::Direction curr_direction = game.GetSnake().front().direction;
+    while (!game.GameOver()) {
+        snake::game::Direction new_direction = snake::graphics::ReadKeypad();
+
+        /* update the direction only if the user provided one */
+        if (new_direction != snake::game::Direction::kNone) {
+            curr_direction = new_direction;
+        }
+        game.Tick(curr_direction);
+        snake::graphics::DrawSnakeScreen(game);
+    }
+    snake::graphics::DisableInputDelay();
+}
 
 int main() {
-    initscr();
+    /* configure the screen */
+    snake::game::ScreenDimension screen_dim = snake::graphics::InitScreen();
 
-    int row = 0;
-    int col = 0;
-    getmaxyx(stdscr, row, col);
+    /* draw the initial game screen */
+    snake::game::SnakeGame game(screen_dim);
+    snake::graphics::DrawSnakeScreen(game);
 
-    std::string mesg("say hello to the snake");
-    mvprintw(row / 2, (col - static_cast<int>(mesg.size())) / 2, "%s",
-             mesg.c_str());
+    RunGameLoop(game);
 
-    refresh();
-    getch();
-    endwin();
+    /* show the game over screen with the score and exit */
+    snake::graphics::DrawGameOverScreen(game);
+    snake::graphics::TerminateScreen();
 
     return 0;
 }
